104b2d1c94727f604580cdacce36ee9323b3590d
Wander Feliz <wxfeliz@octagono.com.do>
2020-06-24 13:38:16 -0400

    Update __init__.py

--- octagono_gps/models/__init__.py
+++ octagono_gps/models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+from . import product_template
+from . import product_product
 from . import octagono
 from . import octagono_model
-from . import product_product
-from . import product_template
 from . import res_company
 from . import res_partner
 from . import stock
