dcce595cdbd2ea7f0f2277abde75df850d717b0b
Octagonosrl <32674200+octagonosrl@users.noreply.github.com>
2025-03-03 21:41:44 -0400

    Update __init__.py

--- octagono_gps/models/__init__.py
+++ octagono_gps/models/__init__.py
@@ -4,6 +4,6 @@ from . import product_product
 # from . import octagono
 from . import octagono_model
 from . import res_company
-from . import res_partner
+# from . import res_partner
 # from . import stock
 # from . import account_move
