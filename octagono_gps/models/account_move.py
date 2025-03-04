62ec4c46191cf6850356794e878b24411cbee730
Octagonosrl <32674200+octagonosrl@users.noreply.github.com>
2025-03-03 21:15:05 -0400

    4

--- octagono_gps/models/account_move.py
+++ octagono_gps/models/account_move.py
@@ -2,6 +2,6 @@ from odoo import fields, models
 
 
 class AccountMoveLine(models.Model):
-    _inherit = "account.invoice"
+    _inherit = "account.move"
 
     account = fields.Char(related="partner_id.Cuenta_GPS", store=True, string="Cuenta")
