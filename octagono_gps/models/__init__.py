002a72994cad365b8dc62feb286d41e7a0f21baa
Octagonosrl <32674200+octagonosrl@users.noreply.github.com>
2025-03-03 21:20:08 -0400

    Update __init__.py

--- octagono_gps/models/__init__.py
+++ octagono_gps/models/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from . import product_template
 from . import product_product
-# from . import octagono
-# from . import octagono_model
+from . import octagono
+from . import octagono_model
 # from . import res_company
-# from . import res_partner
+from . import res_partner
 # from . import stock
 # from . import account_move
