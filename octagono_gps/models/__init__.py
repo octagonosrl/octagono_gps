0cd1f6113ab96f9a2ee861efbbacd809d8d914c6
Octagonosrl <32674200+octagonosrl@users.noreply.github.com>
2025-03-03 20:36:09 -0400

    1

--- octagono_gps/models/__init__.py
+++ octagono_gps/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import product_template
 from . import product_product
-from . import res_partner
+# from . import res_partner
 # from . import octagono
 # from . import octagono_model
 # from . import res_company
