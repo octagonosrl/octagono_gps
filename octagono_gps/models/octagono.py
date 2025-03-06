e748e7659345a6f7d640daadd7cce14d3b0078a1
Octagonosrl <32674200+octagonosrl@users.noreply.github.com>
2025-03-06 18:29:28 -0400

    Update octagono.py

--- octagono_gps/models/octagono.py
+++ octagono_gps/models/octagono.py
@@ -80,7 +80,7 @@ class OctagonoGPS(models.Model):
     # partner_invoice_id = fields.Many2one('res.partner', string='Invoice Address', readonly=True, required=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, help="Invoice address for current sales order.")
     # partner_shipping_id = fields.Many2one('res.partner', string='Dirección de entrega', readonly=True, required=True,  states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, help="Delivery address for current sales order.")
     # pricelist_id = fields.Many2one('product.pricelist', string='Pricelist', required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, help="Pricelist for current sales order.")
-    # currency_id = fields.Many2one("res.currency", related='pricelist_id.currency_id', string="Currency", readonly=True, required=True, store=True)
+    currency_id = fields.Many2one("res.currency", related='pricelist_id.currency_id', string="Currency", readonly=True, required=True, store=True)
     # order_line = fields.One2many('octagono.gps.line', 'order_id', string='Order Lines', states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True, auto_join=True)
     # note = fields.Text('Nota', default=_default_note, tracking=True)
     # payment_term_id = fields.Many2one('account.payment.term', string='Payment Terms')
